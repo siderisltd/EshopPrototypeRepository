b86f4bcbc601d09ca75718e054c3a1671ff85ffb
lexsideris@gmail.com <lexsideris@gmail.com>
2016-04-29 19:40:40 +0300

    Added ResourcesExtensions

---
+++ Eshop/Web/Eshop.Web.Infrastructure/ResourcesExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Eshop.Web.Infrastructure
+{
+    using System;
+    using System.Reflection;
+    using System.Resources;
+    using GlobalConstants;
+
+    public static class ResourcesExtensions
+    {
+
+        public static string GetCategoryResourceByName(this string name)
+        {
+            Assembly assembly = Assembly.Load(AssembliesConstants.GLOBAL_RESOURCES_ASSEMBLY);
+            ResourceManager rm = new ResourceManager(AssembliesConstants.CATEGORIES_RESOURCES_ASSEMBLY, assembly);
+            rm.IgnoreCase = true;
+            var resource = rm.GetString(name) ?? name;
+
+            return resource;
+        }
+    }
+}
