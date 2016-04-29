68dd2efcca30235e32c2ff61d0aa0af9301714b7
lexsideris@gmail.com <lexsideris@gmail.com>
2016-04-29 19:46:13 +0300

    Added ItemsController

---
+++ Eshop/Web/Eshop.Web/Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+﻿namespace Eshop.Web.Controllers
+{
+    using System.Web.Mvc;
+    using System.Linq;
+    using GlobalConstants;
+    using Infrastructure.Mapping.Contracts;
+    using Models.Items;
+    using Services.Data.Contracts;
+
+    public class ItemsController : BaseController
+    {
+        private readonly ICategoriesService categoriesService;
+        private readonly IItemsService itemsService;
+
+        public ItemsController(ICategoriesService categoriesService, IItemsService itemsService)
+        {
+            this.categoriesService = categoriesService;
+            this.itemsService = itemsService;
+        }
+
+        [HttpGet]
+        public ActionResult ByCategory(string name, int page = CommonConstants.DEFAULT_PAGE, int pageSize = CommonConstants.DEFAULT_PAGE_SIZE)
+        {
+            var foundItems = this.itemsService.FindByCategory(name).To<ItemViewModel>().ToList();
+
+            // TODO: define how the results will be displayed
+            return null;
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(ItemBindingModel model)
+        {
+            return null;
+        }
+    }
+}
