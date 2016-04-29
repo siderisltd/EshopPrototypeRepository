8a87bfe51fc376f26319a7b4a92a2fc9dcf8048c
lexsideris@gmail.com <lexsideris@gmail.com>
2016-04-29 19:38:23 +0300

    Added GlobalConstants

---
+++ Eshop/GlobalConstants/AssembliesConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace GlobalConstants
+{
+    public class AssembliesConstants
+    {
+        public const string GLOBAL_RESOURCES_ASSEMBLY = "App_GlobalResources";
+
+        public const string SERVICES_DATA_ASSEMBLY = "Eshop.Services.Data";
+
+        public const string SERVICES_COMMON_ASSEMBLY = "Eshop.Services.Common";
+
+        public const string SERVICES_WEB_ASSEMBLY = "";
+
+        public const string CATEGORIES_RESOURCES_ASSEMBLY = "Resources.CategoriesResources";
+    }
+}
---
+++ Eshop/GlobalConstants/CommonConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace GlobalConstants
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CommonConstants
+    {
+        public const int DEFAULT_PAGE_SIZE = 10;
+
+        public const int DEFAULT_PAGE = 1;
+    }
+}
